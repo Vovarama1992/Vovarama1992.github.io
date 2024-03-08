d34145ead145d3c3d514d051253cccca35412d91
Vovarama1992 <vovayhh9988@gmail.com>
2024-03-08 17:33:54 +0300

    linterChange

--- js/script.js
+++ js/script.js
@@ -26,41 +26,6 @@ let temperDiff = 0;
 let level = 1;
 tips.style.display = "none";
 statistic.style.display = "none";
-function think() {
-    
-    if (!mouseOn) {
-        return;
-    };
-    tips.textContent = "more time to think";
-    setTimeout(() => {
-        tips.textContent = "the last element destroys its relatives in the row";
-}, 1500);
-}
-function increase() {
-   
-   if (!mouseOn) {
-    return;
-};
-tips.textContent = "cause fire increases the temperature";
-     setTimeout(think, 1500);
-}
-function too() {
-    
-    if (!mouseOn) {
-        return;
-    };
-    tips.textContent = "... fires too!";
-    setTimeout(increase, 1500);
-}
-function showTips() {
-    tips.style.display = "flex";
-    if (!mouseOn) {
-        return;
-    };
-
-   tips.textContent = "save palms, but";
-   setTimeout(too, 1500);
-}
 function tipsShow(stop, index) {
     let arr = ["save palms, but...",
                "...fires too!",
